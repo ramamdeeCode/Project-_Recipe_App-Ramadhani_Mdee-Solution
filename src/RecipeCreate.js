033fa57f7da81391e0ac591de654f0b6f28b7dfd
Rama <86864383+ramamdeeCode@users.noreply.github.com>
2022-06-30 00:26:26 -0400

    set up form input fields on create components

--- src/RecipeCreate.js
+++ src/RecipeCreate.js
@@ -9,9 +9,33 @@ function RecipeCreate() {
   return (
     <form name="create">
       <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Cuisine</th>
+            <th>Photo</th>
+            <th>Ingridients</th>
+            <th>Preparation</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
         <tbody>
           <tr>
-            <td></td>
+            <td>
+              <input name="name" placeholder="Name" />
+            </td>
+            <td>
+              <input name="cuisine" placeholder="Cuisine" />
+            </td>
+            <td>
+              <input name="photo" placeholder="Photo" />
+            </td>
+            <td>
+              <textarea name="ingredients" placeholder="Ingredients" />
+            </td>
+            <td>
+              <textarea name="preparation" placeholder="Preparation" />
+            </td>
             <td>
               <button type="submit">Create</button>
             </td>
--- src/RecipeRead.js
+++ src/RecipeRead.js
@@ -26,24 +26,4 @@ function RecipeRead({ deleteRecipe, recipes }) {
   );
 }
 
-// const tableRows = recipes.map(
-//   ({ name, cuisine, photo, ingredients, preparation }, index) => {
-//     return (
-//       <tr key={index}>
-//         <td>{name}</td>
-//         <td>{cuisine}</td>
-//         <td>
-//           <img src={photo} />
-//         </td>
-//         <td>{ingredients}</td>
-//         <td>{preparation}</td>
-//         <td>
-//           <button name="delete" onClick={() => deleteRecipe(index)}>
-//             Delete
-//           </button>
-//         </td>
-//       </tr>
-//     );
-//   }
-// );
 export default RecipeRead;
